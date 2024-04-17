e56ae36719f0132c997b62988a4f11080487ec77
= <=>
2024-04-17 20:33:10 +0200

    Fin clase Miercoles

--- UD 8 - Vue 3 js/mi-primer-vue/src/App.vue
+++ UD 8 - Vue 3 js/mi-primer-vue/src/App.vue
@@ -1,39 +1,30 @@
+<script setup>
+    const nombre = "Vue 3";
+    const objetoFruta = 
+        {
+            name: "Manzana",
+            price: "$1.00",
+            description: "Una manzana",
+        };
+</script>
+
+<template>
+  <h1>Hola {{ nombre }}</h1>
+  <ul>
+    <!-- El :key ha de ser algo que no se repita en el elemento-->
+      <li v-for=" (valor, propiedad ) in objetoFruta"> 
+        {{ propiedad }} : {{ valor }}
+      </li>
+  </ul>
+
+</template>
+
 <style>
   h1 {
     color: red;
   }
 </style>
 
-<script setup>
-  const nombre = "Luis";
-  const estilo = "color:blue";
-  const estado = true;
-  const colores = ["blue", "red", "green"];
-  const contador = 6;
-</script>
-
-<template>
-  <h1>Hola {{ nombre.toUpperCase() }}</h1>
-  <!-- <h2 :style="estilo">Hola a toda la clase de DAM 1. Soy azul</h2>
-  <h2>{{ activo ? "Estoy activo" : "Estoy inactivo" }}</h2>
-  <h2 v-bind:style="`color: ${colores[2]}`"> Color con interpolación.</h2>
-  <h2>Contador: {{ contador + 2 }}</h2>
-
-  Esto no se puede hacer
-  <p>{{ if (activo) { return contador} }}</p>-->
-
-  
-  <p v-if="nombre==='Alberto'">Mi nombre es Alberto</p>
-
-  <h1 v-show="contador===1">El contador el igual a 1</h1>
-
-  <div v-if="estado === true">
-    <p>Hola estoy activo</p>
-    <p>Me mola la clase de DAM 1 Hoy</p>
-  </div>
-  <p v-else-if="estado === false">Hola NO estoy activo</p>
-  <p v-else>Hola, aquí no voy a entrar NUNCA</p>
-</template>
 
 
 
