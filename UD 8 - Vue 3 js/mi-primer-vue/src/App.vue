c9d78e5b0dacf9be305c521d42478a52760b9c0a
= <=>
2024-04-19 18:25:47 +0200

    Fin clase Viernes

--- UD 8 - Vue 3 js/mi-primer-vue/src/App.vue
+++ UD 8 - Vue 3 js/mi-primer-vue/src/App.vue
@@ -1,24 +1,50 @@
 <script setup>
+    import { ref, computed } from 'vue';
+
     const nombre = "Vue 3";
 
-    //método hacerClick
-    const hacerClick = ( mensaje ) => {
-      console.log( mensaje );
-    }
+    // Voy a convertir el contador en una variable reactiva
+    const contador = ref(0);
+
+    const incrementar = () => {
+      //contador++;
+      contador.value++;
+    };
+    const decrementar = () => {
+      contador.value--;
+    };
+    const resetear = () => {
+      contador.value = 0;
+    };
+
+    const claseContador = computed( ()=>{
+      if ( contador.value === 0 ) return 'zero';
+      return contador.value > 0 ? 'positive' : 'negative';
+    });
+
 
 </script>
 
 <template>
-  <h1>Hola {{ nombre }}</h1>
-  <button v-on:click.left="hacerClick('Click desde el Botón 1')">Boton 1</button>
-  <button @click.middle="hacerClick('Click desde el botón 2')">Botón 2</button>
-  <button @click.right="hacerClick('Click desde el botón 2')">Botón 3</button>
-
+    <h1>Hola {{ nombre }}</h1>
+    <h2 :class="claseContador">{{ contador }}</h2>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    <button @click="resetear">Resetear</button>
 </template>
 
 <style>
   h1 {
     color: red;
+  }
+  .negative{
+    color: red;
+  }
+  .positive{
+    color: green;
+  }
+  .zero{
+    color: peru;
   }
 </style>
 
