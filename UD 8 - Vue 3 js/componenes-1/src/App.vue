ad50b2b8816b903f127a8d4781502bc89209e75b
= <=>
2024-05-08 21:11:50 +0200

    Fin clase Miercoles

---
+++ UD 8 - Vue 3 js/componenes-1/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { ref } from 'vue';
+import BlogPost from './components/BlogPost.vue';
+  import ButtonCounter from './components/ButtonCounter.vue';
+
+  // 1 - Me creo un array de objetos
+  const posts = ref([
+    { titulo: 'Post 01', id: 1, body:'descripcion 01'},
+    { titulo: 'Post 02', id: 2, body:'descripcion 02'},
+    { titulo: 'Post 03', id: 3, body:'descripcion 03'},
+    { titulo: 'Post 04', id: 4, body:'descripcion 04'},
+    { titulo: 'Post 05', id: 5, body:'descripcion 05'},
+    { titulo: 'Post 06', id: 6, },
+  ])
+</script>
+
+<template>
+  <div class="container">
+    <h1>Componentes</h1>
+    <ButtonCounter ></ButtonCounter>
+    <!--
+    <BlogPost titulo="Post 01" :id="1" body="Esto es mi descripción 01" colorText="primary"/>
+    <BlogPost titulo="Post 02" :id="2" body="Esto es mi descripción 02" colorText="warning"/>
+    <BlogPost titulo="Post 03" :id="3" body="Esto es mi descripción 03" colorText="danger"/>
+    <BlogPost titulo="Post 04" :id="4" />
+    -->
+    <BlogPost v-for="post in posts" :key="post.id" :titulo="post.titulo" :id="post.id" :body="post.body"/>
+  </div>
+
+  
+</template>
---
+++ UD 8 - Vue 3 js/componenes-1/src/components/BlogPost.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+//defineProps(['titulo', 'body', 'id', 'colorText']);
+defineProps({
+    titulo: String,
+    id: Number,
+    body: {
+        type: String,
+        default: "Sin descripción",
+    }
+})
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <!--<h5 class="card-title" :class="`text-${colorText}`"> {{ id }} - {{ titulo }}</h5>-->
+            <h5> {{ id }} - {{ titulo }}</h5>
+            <p>{{ body }}</p>
+        </div>
+    </div>
+</template>
